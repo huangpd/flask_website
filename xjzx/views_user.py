521330e3143cbd4c1aa7076e9cf16d9b3616806b
kangyuqi <553267084@qq.com>
2018-05-30 18:01:17 +0800

    4.我的关注，我的收藏，新闻列表，密码修改

--- xjzx/views_user.py
+++ xjzx/views_user.py
@@ -5,11 +5,12 @@ from flask import Blueprint, session, make_response, jsonify, render_template, r
 from flask import current_app
 from flask import request
 
-from models import UserInfo, db
+from models import UserInfo, db, NewsInfo
 
 user_blueprint = Blueprint('user', __name__, url_prefix='/user')
 
 
+# 图片验证
 @user_blueprint.route('/image_yzm')
 def image_yzm():
     from utils.captcha.captcha import captcha
@@ -26,6 +27,7 @@ def image_yzm():
     return response
 
 
+# 短信验证
 @user_blueprint.route('/sms_yzm')
 def sms_yzm():
     # 获取数据：手机号，图片码
@@ -53,6 +55,7 @@ def sms_yzm():
     return jsonify(result=2)
 
 
+# 注册
 @user_blueprint.route('/register', methods=['POST'])
 def register():
     # 1.接收数据
@@ -98,6 +101,7 @@ def register():
     return jsonify(result=7)
 
 
+# 登陆
 @user_blueprint.route('/login', methods=['POST'])
 def login():
     # 接收
@@ -125,6 +129,7 @@ def login():
         return jsonify(result=2)
 
 
+# 注销
 @user_blueprint.route('/logout', methods=['POST'])
 def logout():
     session.pop('user_id')
@@ -143,6 +148,7 @@ def login_required(f):
     return fun2
 
 
+# 首页
 @user_blueprint.route('/')
 @login_required
 def index():
@@ -153,7 +159,6 @@ def index():
                            title='用户中心')
 
 
-# 定义用户视图
 # 基本资料
 @user_blueprint.route('/base', methods=['GET', 'POST'])
 @login_required
@@ -187,6 +192,7 @@ def base():
         return jsonify(result=1)
 
 
+# 头像设置
 @user_blueprint.route('/pic', methods=['GET', 'POST'])
 @login_required
 def pic():
@@ -207,31 +213,136 @@ def pic():
         return jsonify(result=1, avatar_url=user.avatar_url)
 
 
-@user_blueprint.route('/follow', methods=['GET', 'POST'])
+# 我的关注
+@user_blueprint.route('/follow')
 @login_required
 def follow():
-    pass
+    user = UserInfo.query.get(session['user_id'])
+    # 接受当前页码值参数
+    page = int(request.args.get('page', '1'))
+    # 对数据进行分页
+    pagination = user.follow_user.paginate(page, 4, False)
+    # 获取当前页的数据
+    user_list = pagination.items
+    # 获取总页数
+    total_page = pagination.pages
+
+    return render_template(
+        'news/user_follow.html',
+        page=page,
+        user_list=user_list,
+        total_page=total_page
+    )
 
 
+# 修改密码
 @user_blueprint.route('/pwd', methods=['GET', 'POST'])
 @login_required
 def pwd():
-    pass
+    if request.method == 'GET':
+        # 为提供页面，输入密码
+        return render_template('news/user_pass_info.html')
+    elif request.method == 'POST':
+        # 接收数据，进行修改
+        msg = '修改成功'
+        # 1.接收数据
+        dict1 = request.form
+        current_pwd = dict1.get('current_pwd')
+        new_pwd = dict1.get('new_pwd')
+        new_pwd2 = dict1.get('new_pwd2')
+        print(current_pwd)
+        print(new_pwd)
+        print(new_pwd2)
+
+        # 2.进行验证
+        if not all([current_pwd, new_pwd, new_pwd2]):
+            return render_template(
+                'news/user_pass_info.html',
+                msg='密码不能为空'
+            )
+        import re
+        if not re.match(r'[a-zA-Z0-9_]{6,20}', current_pwd):
+            return render_template(
+                'news/user_pass_info.html',
+                msg='????'
+            )
+        if not re.match(r'[a-zA-Z0-9_]{6,20}', new_pwd):
+            return render_template(
+                'news/user_pass_info.html',
+                msg='新密码格式应为：长度6-20,由字母、数字和下划线组成'
+            )
+        if new_pwd != new_pwd2:
+            return render_template(
+                'news/user_pass_info.html',
+                msg='两次输入的密码不一致'
+            )
+
+        user = UserInfo.query.get(session['user_id'])
+        print(user.check_pwd(current_pwd))
+        if not user.check_pwd(current_pwd):
+            return render_template(
+                'news/user_pass_info.html',
+                msg='当前密码错误'
+            )
+        # 3.修改
+        user.password = new_pwd
+        # 4.提交到数据库
+        db.session.commit()
+        # 5.响应
+        return render_template(
+            'news/user_pass_info.html',
+            msg='密码修改成功'
+        )
 
 
-@user_blueprint.route('/collect', methods=['GET', 'POST'])
+# 我的收藏
+@user_blueprint.route('/collect')
 @login_required
 def collect():
-    pass
+    user_id = session['user_id']
+    user = UserInfo.query.get(user_id)
+    # 接受当前页码值参数
+    page = int(request.args.get('page', '1'))
+    # 对数据进行分页
+    pagination = user.news_collect.order_by(NewsInfo.id.desc()).paginate(page, 6, False)
+    # 获取当前页的数据
+    news_list = pagination.items
+    # 获取总页数
+    total_page = pagination.pages
+
+    return render_template(
+        'news/user_collection.html',
+        page=page,
+        news_list=news_list,
+        total_page=total_page
+    )
 
 
+# 新闻发布
 @user_blueprint.route('/release', methods=['GET', 'POST'])
 @login_required
 def release():
-    pass
+    return render_template('news/user_news_release.html')
 
 
+# 新闻列表
 @user_blueprint.route('/newslist', methods=['GET', 'POST'])
 @login_required
 def newslist():
-    pass
+    user_id = session['user_id']
+    user = UserInfo.query.get(user_id)
+    # 接受当前页码值参数
+    page = int(request.args.get('page', '1'))
+    # 对数据进行分页
+    pagination = user.news.order_by(NewsInfo.update_time.desc()).paginate(page, 6, False)
+    # 获取当前页的数据
+    news_list = pagination.items
+    # 获取总页数
+    total_page = pagination.pages
+
+    return render_template(
+        'news/user_news_list.html',
+        page=page,
+        news_list=news_list,
+        total_page=total_page
+    )
